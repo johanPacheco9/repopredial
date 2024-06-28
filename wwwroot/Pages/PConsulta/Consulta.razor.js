4142d94859db81310313f14ff3fb756f1a671280
johanPacheco9 <johanpach9@gmail.com>
2024-06-28 12:54:16 -0500

    'agregar'

---
+++ wwwroot/Pages/PConsulta/Consulta.razor.js
@@ -0,0 +1,15 @@
+﻿function downloadFileFromStream(fileName, contentStreamReference) {
+    contentStreamReference.arrayBuffer().then(arrayBuffer => {
+        const blob = new Blob([arrayBuffer]);
+        const url = URL.createObjectURL(blob);
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName ?? '';
+        anchorElement.click();
+        anchorElement.remove();
+        URL.revokeObjectURL(url);
+    });
+}
+
+window.downloadFileFromStream = downloadFileFromStream;
+
